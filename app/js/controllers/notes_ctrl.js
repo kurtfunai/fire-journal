f8b9600f91612a37305c5dce388944f59ff3d92c
Kurt Funai <jesuiskurt@gmail.com>
2014-03-30 20:21:39 -0400

    Added initial Firebase interaction using a basic form

--- app/js/controllers/notes_ctrl.js
+++ app/js/controllers/notes_ctrl.js
@@ -1,4 +1,11 @@
 angular.module('journal')
-.controller('NotesCtrl', function($scope) {
-  $scope.notes = ['some','notes', 'here'];
+.controller('NotesCtrl', function($scope, $firebase) {
+  var notesRef = new Firebase("https://fire-journal.firebaseio.com/notes");
+  $scope.notes = $firebase(notesRef);
+  $scope.note = {};
+
+  $scope.createNote = function(note) {
+    $scope.notes.$add($scope.note);
+    $scope.note = {};
+  }
 })
