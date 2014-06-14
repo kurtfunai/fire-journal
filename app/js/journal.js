acd6767ff5b45c0759de4c3a2c2d7e3b09a9f382
Kurt Funai <jesuiskurt@gmail.com>
2014-06-14 16:33:12 -0400

    Added daily task completion for team. Track progress/streaks for people.

--- app/js/journal.js
+++ app/js/journal.js
@@ -1,4 +1,14 @@
-var app = angular.module('journal', ['ui.router', 'firebase']);
+var app = angular.module('journal', ['ui.router', 'kf-moment', 'firebase']);
+
+angular.module('kf-moment', []).factory('$moment', ['$window',
+  function ($window) {
+    if (typeof $window.moment === 'undefined') {
+      throw 'moment.js not found.';
+    }
+    return $window.moment;
+  }
+]);
+
 var viewsPath = 'app/views';
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -12,6 +22,11 @@ app.config(function($stateProvider, $urlRouterProvider) {
       url: "/users/sign_up",
       templateUrl: viewsPath+"/auth/sign_up.html",
       controller: "AuthCtrl"
+    })
+    .state('people', {
+      url: "/people",
+      templateUrl: viewsPath+"/people/index.html",
+      controller: "PeopleCtrl"
     })
     .state('notes', {
       url: "/notes",
